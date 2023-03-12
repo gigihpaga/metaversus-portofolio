dd8a7a82af150990c66735b9ff7ae15f691edbf5
gigihpaga <gigihpaga@gmail.com>
2023-03-13 00:47:51 +0700

    create file import-export for images 🗄️

---
+++ constants/images.js
@@ -0,0 +1,61 @@
+import arrowDown from '../public/arrow-down.svg';
+import arrow from '../public/arrow.svg';
+import cover from '../public/cover.png';
+import facebook from '../public/facebook.svg';
+import favicon from '../public/favicon.ico';
+import getStarted from '../public/get-started.png';
+import headset from '../public/headset.svg';
+import instagram from '../public/instagram.svg';
+import linkedin from '../public/linkedin.svg';
+import map from '../public/map.png';
+import menu from '../public/menu.svg';
+import people01 from '../public/people-01.png';
+import people02 from '../public/people-02.png';
+import people03 from '../public/people-03.png';
+import planet01 from '../public/planet-01.png';
+import planet02 from '../public/planet-02.png';
+import planet03 from '../public/planet-03.png';
+import planet04 from '../public/planet-04.png';
+import planet05 from '../public/planet-05.png';
+import planet06 from '../public/planet-06.png';
+import planet07 from '../public/planet-07.png';
+import planet08 from '../public/planet-08.png';
+import planet09 from '../public/planet-09.png';
+import search from '../public/search.svg';
+import stamp from '../public/stamp.png';
+import twitter from '../public/twitter.svg';
+import vercel from '../public/vercel.svg';
+import vrpano from '../public/vrpano.svg';
+import whatsNew from '../public/whats-new.png';
+
+export {
+    arrowDown,
+    arrow,
+    cover,
+    facebook,
+    favicon,
+    getStarted,
+    headset,
+    instagram,
+    linkedin,
+    map,
+    menu,
+    people01,
+    people02,
+    people03,
+    planet01,
+    planet02,
+    planet03,
+    planet04,
+    planet05,
+    planet06,
+    planet07,
+    planet08,
+    planet09,
+    search,
+    stamp,
+    twitter,
+    vercel,
+    vrpano,
+    whatsNew,
+};
